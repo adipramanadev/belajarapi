52a63eb312d86d915ef4b8bb985dfea70f1c634c
adipramandev <herryprasetyo.dev@gmail.com>
2023-12-20 20:28:04 +0700

    done gender

--- lib/tambah.dart
+++ lib/tambah.dart
@@ -11,10 +11,24 @@ class AddPage extends StatefulWidget {
 }
 
 class _AddPageState extends State<AddPage> {
+  //dropdown gender
+  String? _selectGender;
   //deklarasi variabel TextField
   TextEditingController _txtNama = TextEditingController();
   TextEditingController _txtEmail = TextEditingController();
   TextEditingController _txtPassword = TextEditingController();
+
+  //dropdown gender
+  List<DropdownMenuItem<String>> _genderItems = [
+    DropdownMenuItem(
+      value: "L",
+      child: Text("Laki-laki"),
+    ),
+    DropdownMenuItem(
+      value: "P",
+      child: Text("Perempuan"),
+    )
+  ];
 
   //function future
   void tambahData(
@@ -31,6 +45,7 @@ class _AddPageState extends State<AddPage> {
         'users_nm': _txtNama.text,
         'users_email': _txtEmail.text,
         'users_pass': _txtPassword.text,
+        'users_pict': _selectGender,
       },
     ).then((response) {
       print('Reponse status: ${response.statusCode}');
@@ -89,6 +104,38 @@ class _AddPageState extends State<AddPage> {
                   hintText: "Masukkan Email",
                   icon: Icon(Icons.mail),
                 ),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              height: 50.0,
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8.0),
+                border: Border.all(color: Colors.grey),
+              ),
+              child: Row(
+                children: <Widget>[
+                  Icon(Icons.person,
+                      color: Colors.grey), // Ikon yang Anda pilih
+                  SizedBox(width: 10), // Jarak antara ikon dan dropdown
+                  Expanded(
+                    child: DropdownButton<String>(
+                      isExpanded: true,
+                      value: _selectGender,
+                      underline: SizedBox(),
+                      hint: Text("Pilih Jenis Kelamin"),
+                      items: _genderItems,
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          _selectGender = newValue;
+                        });
+                      },
+                    ),
+                  ),
+                ],
               ),
             ),
             SizedBox(
