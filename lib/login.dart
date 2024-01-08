cb5df85e92e507821f0bc3c2ffe7ae6a78f79b40
adipramandev <herryprasetyo.dev@gmail.com>
2024-01-08 20:16:30 +0700

    loginDOne

--- lib/login.dart
+++ lib/login.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import "package:http/http.dart" as http;
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -8,6 +11,35 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  TextEditingController txtEmail = TextEditingController();
+  TextEditingController txtPassword = TextEditingController();
+
+  //function login
+  Future<void> login(String email, String password) async {
+    String token = "TESTING";
+    var url = "https://dev-api.arminadaily.id/mobiledev/auth/";
+    var response = await http.post(
+      Uri.parse(url),
+      headers: {
+        "token": "$token",
+      },
+      body: {
+        'users_email': email,
+        'users_pass': password,
+      },
+    );
+    //status response
+    if (response.statusCode == 200) {
+      var data = json.decode(response.body);
+      if (data['status'] == 'true' && data['responseCode'] == '00') {
+        //login sukses
+        print("login Sukses: $data['data]");
+      } else {
+        print("Error: $response.reasonPharase");
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -52,6 +84,59 @@ class _LoginState extends State<Login> {
                         .copyWith(color: Colors.blue),
                   ),
                   const SizedBox(height: 30),
+                  TextFormField(
+                    controller: txtEmail,
+                    decoration: InputDecoration(
+                      fillColor: Colors.white,
+                      filled: true,
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide.none,
+                        borderRadius: BorderRadius.circular(
+                          10.0,
+                        ),
+                      ),
+                      hintText: "Masukkan Email Anda ...",
+                      isDense: true,
+                      prefixIcon: Icon(
+                        Icons.email,
+                      ),
+                    ),
+                  ),
+                  TextFormField(
+                    controller: txtPassword,
+                    decoration: InputDecoration(
+                      fillColor: Colors.white,
+                      filled: true,
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide.none,
+                        borderRadius: BorderRadius.circular(
+                          10.0,
+                        ),
+                      ),
+                      hintText: "Masukkan Password Anda ...",
+                      isDense: true,
+                      prefixIcon: Icon(
+                        Icons.lock,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      login(txtEmail.text, txtPassword.text);
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                    ),
+                    child: Text(
+                      "Login ",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
                 ],
               ),
             ),
